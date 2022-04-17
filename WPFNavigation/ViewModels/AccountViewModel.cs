91e1e5a2a7bae7f18cf406201be1c943ae6ee07f
Alvin Yanson <yansonalvin29@gmail.com>
2022-04-17 10:59:05 +0800

    Add project files.

---
+++ WPFNavigation/ViewModels/AccountViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFNavigation.Commands;
+using WPFNavigation.Stores;
+
+namespace WPFNavigation.ViewModels
+{
+    public class AccountViewModel : ViewModelBase
+    {
+        public string WelcomeMessage => "Welcome to Account Page";
+        public ICommand NavigateHomeCommand { get; }
+
+
+        public AccountViewModel(NavigationStore navigationStore)
+        {
+            NavigateHomeCommand = new NavigateCommand<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore));
+        }
+
+    }
+}
---
+++ WPFNavigation/ViewModels/HomeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFNavigation.Commands;
+using WPFNavigation.Stores;
+
+namespace WPFNavigation.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+
+        public string WelcomeMessage => "Welcome Alvin Yanson";
+
+        public ICommand NavigateAccountCommand { get; }
+
+
+        public HomeViewModel(NavigationStore navigationStore)
+        {
+            NavigateAccountCommand = new NavigateCommand<AccountViewModel>(navigationStore, () => new AccountViewModel(navigationStore));
+        }
+    }
+}
